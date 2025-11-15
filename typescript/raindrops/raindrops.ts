985250b5d0055f0b5317223e136f651402e236a7
BayerTobias <tbayer2@gmx.de>
2025-11-15 10:11:19 +0100

    raindrops

---
+++ typescript/raindrops/raindrops.ts
@@ -0,0 +1,9 @@
+export function convert(number: number): string {
+  let response = "";
+
+  if (number % 3 === 0) response += "Pling";
+  if (number % 5 === 0) response += "Plang";
+  if (number % 7 === 0) response += "Plong";
+
+  return response || String(number);
+}
