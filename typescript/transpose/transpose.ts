b2c12df06326d34ab651395070f88e7fb68bb0c0
BayerTobias <tbayer2@gmx.de>
2026-05-19 13:30:36 +0200

    transpose

---
+++ typescript/transpose/transpose.ts
@@ -0,0 +1,26 @@
+export function transpose(input: string[]): string[] {
+  if (input.length === 0) return [];
+
+  const maxLength = Math.max(...input.map((line) => line.length));
+  const result: string[] = [];
+
+  for (let col = 0; col < maxLength; col++) {
+    let line = "";
+
+    for (let row = 0; row < input.length; row++) {
+      const char = input[row][col];
+
+      if (char !== undefined) {
+        line += char;
+      } else {
+        const hasCharLater = input.slice(row + 1).some((l) => col < l.length);
+
+        line += hasCharLater ? " " : "";
+      }
+    }
+
+    result.push(line);
+  }
+
+  return result;
+}
